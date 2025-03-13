1bde5bc404449e2b726804d6611bf9d178476897
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-03-13 16:10:19 +0000

    components for Header, Footer, App

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="App-footer container-fluid text-center mt-auto">
+            <br />
+            <img src="src/assets/Logo-DnD.svg" className="logo img-fluid" />
+            <p>Coded by <a href="https://coderina.dev/">Katerina Walter</a></p>
+            <p className="small">Compatible with the <strong>2024 Player's Handbook</strong> rules. Information from <a href="https://www.dndbeyond.com/">D&D Beyond</a>.</p>
+        </footer>
+    );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="App-header container-fluid pt-4 mb-0 text-center">
+            <h1>Character Sheet Generator</h1>
+        </header>
+    );
+}
